328bf0c67d0385073c8e5523de01d7d90942bd9a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 13:12:00 +0000

    Automatically backed up by Learn

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const SearchResults = ({ results }) => (
   <ul className='search-results'>
-    {results.map((result) => {<li><p>result.description</p></li>})};
+    {results.map((result) => {<li><a href={result.link}>{result.title}</a><p>{result.description}</p></li>})};
   </ul>
 );
 
